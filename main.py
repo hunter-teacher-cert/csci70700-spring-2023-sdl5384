612e5b84ac3de8a758b8ae261b6963b99b1c5f15
Samuel David Lojacono <96607503+sdl5384@users.noreply.github.com>
2023-02-16 21:42:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import random
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+def roll(givenNumberOfSides):
+  dice = []
+  for i in range(0,random.randint(1,10)):
+    dice.append(random.randint(1,givenNumberOfSides))
+
+  return dice
+
+def generateDiceMessageString(diceArray):
+  msg = 'The numbers rolled after rolling ' + str(len(diceArray)) + ' are: '
+  for j in range(0,len(diceArray)):
+    msg = msg + str(diceArray[j]) + ', '
+  return msg
+
+def average(diceArray):
+  sum = 0
+  for k in range(0,len(diceArray)):
+    sum = sum + diceArray[k]
+
+  return sum/len(diceArray)
+
+def standardDeviation():
+  return None
+
+@app.route('/')
+def index():
+    return 'Hello from Flask!'
+
+@app.route('/gameAnalysis/')
+def analysisPage():
+  dice = []
+  dice = roll(20)
+  msg = generateDiceMessageString(dice)
+  return msg
+
+app.run(host='0.0.0.0', port=81)
+
+@app.route('/monopolyDice/')
+def monopolyDiceStats():
+  #render_template()
+  return None
+
+@app.route('/riskStatistics/')
+def riskDiceStats():
+  return None
