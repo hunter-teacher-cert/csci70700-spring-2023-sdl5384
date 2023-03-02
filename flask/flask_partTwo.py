ecaa3ea2f7d7f7d4fbbb2be2b52ba3d9ddc8fe86
Samuel David Lojacono <96607503+sdl5384@users.noreply.github.com>
2023-03-02 19:49:20 +0000

    Added part two here along with some other HTML files that are accommodating it

---
+++ flask/flask_partTwo.py
@@ -0,0 +1,71 @@
+from flask import Flask
+from flask import render_template
+import random as r
+from flask import session, request
+
+app = Flask(__name__)
+app.secret_key="random nonsense"
+
+@app.route('/')
+def welcomePage():
+    if "count" not in session:
+        session["count"] = 0
+    else:
+        session["count"] = session["count"] + 1
+    return render_template("welcome_page.html",session=session)
+
+@app.route('/login/',methods=['GET','POST'])
+def logInToPage():
+    if request.method == "GET":
+        return render_template("login.html",todos=session["todo_list"])
+    else:
+        print(request.form)
+        item = request.form["TODO"]
+        if "todo_list" not in session:
+            session["todo_list"] = []
+            return render_template("login.html",todos=session["todo_list"])
+
+        if request.form['submit'] == 'doit':
+            l = session["todo_list"]
+            l.append(item)
+            session["todo_list"] = l
+            return render_template("login.html",todos=session["todo_list"])
+
+        else:
+            session["todo_list"] = []
+            return render_template("login_html",todos=session["todo_list"])
+
+@app.route('/rollTheDice/')
+def rollTheDice():
+    # msg = "The numbers you rolled are the following: "
+    # die = []
+    # for i in range(0,15):
+    #     die.append(r.randint(1,6))
+    #     msg = msg + "2 " + session["die3"] + session["die4"] + session["die5"] + str(die[i])
+
+    if "count" not in session:
+        session["count"] = 0
+    else:
+        session["count"] = session["count"] + 1
+
+    if "die1" not in session or "die2" not in session or "die3" not in session or "die4" not in session or "die5" not in session:
+        session["die1"] = 0
+        session["die2"] = 0
+        session["die3"] = 0
+        session["die4"] = 0
+        session["die5"] = 0
+    else:
+        session["die1"] = r.randint(1,6)
+        session["die2"] = r.randint(1,6)
+        session["die3"] = r.randint(1,6)
+        session["die4"] = r.randint(1,6)
+        session["die5"] = r.randint(1,6)
+
+    if session["diceAvg"] not in session:
+        session["diceAvg"] = 0
+    else:
+        session["diceAvg"] = session["diceAvg"] + 1
+        
+    return render_template("diceSimulation.html",session=session)
+
+app.run(host='0.0.0.0', port=5000)
